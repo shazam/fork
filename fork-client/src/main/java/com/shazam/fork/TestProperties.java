d5ff66b5adca1515edcedcc55a4988a21bff2200
Janusz Baginski <jbaginski@apple.com>
2019-09-27 15:31:06 +0100

    Makes RevokePermission annotations available at runtime

--- fork-client/src/main/java/com/shazam/fork/TestProperties.java
+++ fork-client/src/main/java/com/shazam/fork/TestProperties.java
@@ -1,12 +1,12 @@
 package com.shazam.fork;
 
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RUNTIME)
 @Target({METHOD})
 public @interface TestProperties {
     String[] keys() default {};
