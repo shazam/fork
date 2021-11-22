b52a0e508b74ca72dd138dfe3b7498772c9cabd6
Francesco Pezzato <fpezzato@apple.com>
2021-11-22 15:07:51 +0000

    Migrates CI from Travis.org to GitHub Actions

--- build-scripts/coverage.sh
+++ build-scripts/coverage.sh
@@ -4,4 +4,4 @@ set -e
 
 ./gradlew -q clean jacocoMergedReport
 cd build-scripts
-java -cp codacy-coverage-reporter-assembly-1.0.5.jar com.codacy.CodacyCoverageReporter -l Java -r ../build/reports/jacoco/jacocoMergedReport/jacocoMergedReport.xml
+java -cp codacy-coverage-reporter-assembly-1.0.5.jar com.codacy.CodacyCoverageReporter -l Java -t $CODACITY_PROJECT_TOKEN -r ../build/reports/jacoco/jacocoMergedReport/jacocoMergedReport.xml
--- build-scripts/deploy_snapshot.sh
+++ build-scripts/deploy_snapshot.sh
@@ -2,23 +2,18 @@
 #
 # Deploy a jar, source jar, and javadoc jar to Sonatype's snapshot repo.
 #
-# Adapted from https://coderwall.com/p/9b_lfq and
-# http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
-SLUG="shazam/fork"
-JDK="openjdk8"
+REPOSITORY="shazam/fork"
 BRANCH="master"
 
 set -e
 
-if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
-  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
-  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [ "$GITHUB_REPOSITORY" != "$REPOSITORY" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$REPOSITORY' but was '$GITHUB_REPOSITORY'."
+elif [ "$GITHUB_IS_PULL_REQUEST" != "false" ]; then
   echo "Skipping snapshot deployment: was pull request."
-elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
-  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+elif [ "$GITHUB_REF_NAME" != "$BRANCH" ]; then
+  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$GITHUB_REF_NAME'."
 else
   echo "Deploying snapshot..."
   ./gradlew clean uploadArchives
