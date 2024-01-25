3ee447dd19441f348814e6f71c80ceff347d7c86
Janusz Baginski <jbaginski@apple.com>
2024-01-25 16:13:32 +0000

    Updates codacy reporter

--- build-scripts/coverage.sh
+++ build-scripts/coverage.sh
@@ -5,4 +5,4 @@ set -e
 ./gradlew -q clean jacocoMergedReport
 cd build-scripts
 ls ../build/reports/jacoco/jacocoMergedReport
-java -jar codacy-coverage-reporter-assembly-13.5.3.jar report -l Java -r "../build/reports/jacoco/jacocoMergedReport/jacocoMergedReport.xml" -t "$CODACY_PROJECT_TOKEN"
+java -jar codacy-coverage-reporter-assembly-13.15.1.jar report -l Java -r "../build/reports/jacoco/jacocoMergedReport/jacocoMergedReport.xml" -t "$CODACY_PROJECT_TOKEN"
